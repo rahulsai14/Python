f3f201dfca54156afe1650ec1b856469c9924be0
rahulsai14 <98695626+rahulsai14@users.noreply.github.com>
2022-01-30 18:59:10 +0530

    Update and rename day1project.py to band-name-generator.py

---
+++ band-name-generator.py
@@ -0,0 +1,4 @@
+print('!!!Welcome to the band name generator!!!')
+city=input('Which city are you from?\n')
+favname=input('What is your fav name?\n')
+print('Your band name: ' + city +' ' +favname)
